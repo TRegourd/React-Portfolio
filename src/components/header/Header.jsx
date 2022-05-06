4710f6c8cb4012068a05b2dbad9b6890ca02c8d9
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-06 09:59:48 +0200

    finished layout of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import { CgScrollH } from "react-icons/cg";
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5>Hi there 👋</h5>
         <h1>Thomas Regourd</h1>
         <h5 className="text-light">Fullstack JS Developer</h5>
@@ -17,7 +17,7 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="Thomas' Profile Picutre" />
         </div>
-        <a href="#contact" className="scroll_down">
+        <a href="#contact" className="scroll__down">
           Scroll Down <CgScrollH />
         </a>
       </div>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,7 +3,7 @@ import { BsLinkedin, BsGithub, BsDribbble } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
-    <div className="header_socials">
+    <div className="header__socials">
       <a href="https://www.linkedin.com/in/thomasregourd/" target="_blank">
         <BsLinkedin />
       </a>
