6061ba87c46bd49ba9a9af3e98e5c9574c2d9202
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-06 11:05:36 +0200

    nav bar ok, TODO links and active

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./nav.css";
+import { TiHomeOutline } from "react-icons/ti";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBookContent } from "react-icons/bi";
+import { BsHandThumbsUp } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav>
+      <a href="">
+        <TiHomeOutline />
+      </a>
+      <a href="">
+        <AiOutlineUser />
+      </a>
+      <a href="">
+        <BiBookContent />
+      </a>
+      <a href="">
+        <BsHandThumbsUp />
+      </a>
+      <a href="">
+        <MdOutlineEmail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
